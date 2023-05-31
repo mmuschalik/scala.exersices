c59815213ca081e82c6cb4f476245f08bc872016
Maurice Muschalik <maurice.muschalik@gmail.com>
2023-06-01 00:27:50 +1000

    cleanup

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,19 +1,16 @@
 
 @main def entry(): Unit =
-  System.out.println(isInterestingTuple(3, 0, 0, List(-2, -1, -5, -3)))
-  return
+  System.out.println(isInterestingTuple(3, List(-2, -1, -5, -3)))
 
+def isInterestingTuple(size: Int, numbers: List[Int]): Boolean =
+  isInterestingTuple(size, 0, 0, numbers)
 
-def  isInterestingTuple(size: Int, count: Int, lastNumber: Int, rest: List[Int]): Boolean =
-    rest
-      .headOption
-      .fold(count >= size)(
-        currentItem =>
+def isInterestingTuple(size: Int, count: Int, lastNumber: Int, rest: List[Int]): Boolean =
+  rest
+    .headOption match
+      case None => count >= size
+      case Some(currentItem) =>
           count >= size
           || isInterestingTuple(size, count, lastNumber, rest.tail)
           || ((count == 0 || lastNumber < currentItem)
                 && isInterestingTuple(size, count + 1, currentItem, rest.tail))
-      )
-
-
-
